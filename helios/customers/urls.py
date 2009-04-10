fad495774a80387a39f68445622af778d0491d1f
Panos Laganakos <panosl@Menzoberanzan.local>
2009-04-10 19:26:34 +0300

    removed irrelevant templates.

--- helios/customers/urls.py
+++ helios/customers/urls.py
@@ -3,10 +3,14 @@ from django.views.generic import list_detail
 from helios.customers.views import order_list
 
 urlpatterns = patterns('',
-	(r'^$', 'helios.customers.views.customer'),
-	url(r'^register/$', 'helios.customers.views.customer', name='customer-register'),
+	url(r'^$', 'helios.customers.views.customer',
+		name='customer'),
+	url(r'^register/$', 'helios.customers.views.customer',
+		name='customer-register'),
 	(r'^orders/$', order_list, {'template_name': 'customer/orders.html', 'template_object_name': 'order'}),
-	(r'^login/$', 'django.contrib.auth.views.login'),
-	(r'^logout/$', 'django.contrib.auth.views.logout'),
+	url(r'^login/$', 'django.contrib.auth.views.login',
+		name='customer-login'),
+	url(r'^logout/$', 'django.contrib.auth.views.logout',
+		name='customer-logout'),
 	(r'^reset-password/$', 'django.contrib.auth.views.password_reset'),
 )
