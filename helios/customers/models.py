20945201fedeae4681356342a4dc0a9068d00725
Panos Laganakos <panosl@Menzoberanzan.local>
2009-05-31 23:26:39 +0300

    translation updates.

--- helios/customers/models.py
+++ helios/customers/models.py
@@ -13,12 +13,16 @@ from helios.location.models import Country
 
 class CustomerProfile(models.Model):
 	user = models.ForeignKey(User, unique=True)
-	phone = models.CharField(_('Phone number'), max_length=30, blank=True)
+	phone = models.CharField(_('phone number'), max_length=30, blank=True)
 	fax = models.CharField(_('Fax'), max_length=30, blank=True)
-	address = models.TextField(_('Address'), blank=True)
-	postal_code = models.CharField(_('Postal code'), max_length=10, blank=True)
-	city = models.CharField(_('City'), max_length=50, blank=True)
+	address = models.TextField(_('address'), blank=True)
+	postal_code = models.CharField(_('postal code'), max_length=10, blank=True)
+	city = models.CharField(_('city'), max_length=50, blank=True)
 	country = models.ForeignKey(Country)
+
+	#class meta:
+	#	verbose_name = _('customer profile')
+	#	verbose_name_plural = _('customer profiles')
 
 	def __unicode__(self):
 		return u'%s (%s)' % (self.user.get_full_name(), self.user.email)
--- helios/location/models.py
+++ helios/location/models.py
@@ -13,9 +13,9 @@ from django.utils.translation import gettext_lazy as _
 class Country(models.Model):
 	name = models.CharField(_('name'), max_length=45)
 
-	class Meta:
-		verbose_name = _('country')
-		verbose_name_plural = _('countries')
+	#class Meta:
+	#	verbose_name = _('country')
+	#	verbose_name_plural = _('countries')
 
 	def __unicode__(self):
 		return self.name
