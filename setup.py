aa34c0a6e4c229e21f5f88d44b846baa3f6a9509
Panos Laganakos <panosl@Menzoberanzan.lan>
2009-11-13 17:58:30 +0200

    added a setup.py file.

---
+++ setup.py
@@ -0,0 +1,18 @@
+#!/usr/bin/env python
+from distutils.core import setup
+
+
+setup(name='helios',
+	version='0.0.4',
+	description='Phaethon\'s e-commerce application.',
+	author='Panos Laganakos',
+	author_email='panos.laganakos@gmail.com',
+	packages=['helios'],
+	classifiers=['Development Status :: 4 - Beta',
+		'Environment :: Web Environment',
+		'Intended Audience :: Developers',
+		'License :: OSI Approved :: BSD License',
+		'Operating System :: OS Independent',
+		'Programming Language :: Python',
+		'Topic :: Utilities'],
+)
