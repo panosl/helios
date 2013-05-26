7113580af3ae73352f7588f51aa091b72b75ab2c
Panos Laganakos <panos.laganakos@gmail.com>
2013-05-26 17:56:51 +0300

    removed unused files.

--- helios/orders/admin.py
+++ helios/orders/admin.py
@@ -1,37 +1,36 @@
 from django.contrib import admin
 from helios.orders.models import Order, OrderLine, OrderStatus
-from helios.store.forms import MyCategoryAdminForm
 from helios.conf import settings
 if settings.IS_MULTILINGUAL:
-	import multilingual
-	admin_info = {
-		'class': multilingual.ModelAdmin,
-		'suffix': '_en'
-	}
+    import multilingual
+    admin_info = {
+        'class': multilingual.ModelAdmin,
+        'suffix': '_en'
+    }
 else:
-	admin_info = {
-		'class': admin.ModelAdmin,
-		'suffix': ''
-	}
+    admin_info = {
+        'class': admin.ModelAdmin,
+        'suffix': ''
+    }
 
 
 class OrderLineAdmin(admin.ModelAdmin):
-	pass
+    pass
 
 
 class OrderLineInline(admin.TabularInline):
-	model = OrderLine
+    model = OrderLine
 
 
 class OrderAdmin(admin.ModelAdmin):
-	date_hierarchy = 'date_time_created'
-	inlines = [OrderLineInline]
-	list_display = ['date_time_created', 'customer', 'status']
-	list_filter = ('status',)
+    date_hierarchy = 'date_time_created'
+    inlines = [OrderLineInline]
+    list_display = ['date_time_created', 'customer', 'status']
+    list_filter = ('status',)
 
 
 class OrderStatusAdmin(admin_info['class']):
-	prepopulated_fields = {'slug': (''.join(['name', admin_info['suffix']]),)}
+    prepopulated_fields = {'slug': (''.join(['name', admin_info['suffix']]),)}
 
 
 admin.site.register(Order, OrderAdmin)
