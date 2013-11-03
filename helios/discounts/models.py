c2558b339df77761de2d0879da34ec5a29a88112
Panos Laganakos <panos.laganakos@gmail.com>
2013-11-03 14:38:18 +0200

    show a better string for discounts.

--- helios/discounts/models.py
+++ helios/discounts/models.py
@@ -7,3 +7,6 @@ from helios.store.models import Category
 class CategoryPercentageDiscount(models.Model):
     category = models.OneToOneField(Category)
     discount = models.DecimalField(_('discount'), max_digits=5, decimal_places=2)
+
+    def __unicode__(self):
+        return u'%s %s%%' % (self.category, self.discount,)
