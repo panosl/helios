db2ae2269d319268f96faf93eef2fddba3344c9f
Panos Laganakos <panos.laganakos@gmail.com>
2016-05-28 01:59:28 +0300

    updated urls for django 1.10.

--- helios/store/urls.py
+++ helios/store/urls.py
@@ -5,40 +5,48 @@ from django.conf.urls import *
 from django.views.generic import TemplateView, ListView
 from helios.store.models import ProductImage
 from helios.conf import settings
-from helios.store.views import CollectionList, ProductDetail, ProductList
+from helios.store.views import *
 from helios.conf import settings
 
 
 module_name, model_name = settings.PRODUCT_MODEL.rsplit('.', 1)
 ProductModel = getattr(import_module(module_name), model_name)
 
-urlpatterns = patterns('helios.store.views',
-    (r'^cart/clear/$', 'cart_clear'),
-    (r'^cart/debug/$', 'cart_debug'),
+urlpatterns = [
+    url(r'^cart/clear/$', cart_clear),
+    url(r'^cart/debug/$', cart_debug),
 
     url(r'^cart/set/(?P<product_id>\d+)/$',
-        'cart_set_quantity',
-        name='store_cart_set_quantity'),
+        cart_set_quantity,
+        name='store_cart_set_quantity'
+    ),
     url(r'^checkout/$',
-        'checkout',
-        name='store_checkout'),
-    url(r'^success/$', 'success',
-        name='store_success'),
+        checkout,
+        name='store_checkout'
+    ),
+    url(r'^success/$',
+        success,
+        name='store_success'
+    ),
     #url(r'^submit-order/$',
         #'submit_order',
-        #name='store_submit_order'),
+        #name='store_submit_order'
+    #),
     url(r'^products/(?P<slug>[-\w]+)/add/$',
-        'product_add',
-        name='store_product_add'),
+        product_add,
+        name='store_product_add'
+    ),
     url(r'^products/(?P<slug>[-\w]+)/remove/$',
-        'product_remove',
-        name='store_product_remove'),
+        product_remove,
+        name='store_product_remove'
+    ),
     url(r'^unshippable/$',
-        'unshippable',
-        name='store_unshippable'),
-)
+        unshippable,
+        name='store_unshippable'
+    ),
+]
 
-urlpatterns += patterns('',
+urlpatterns += [
     url(r'^cart/$',
         TemplateView.as_view(
             template_name='cart.html'
@@ -65,4 +73,4 @@ urlpatterns += patterns('',
         ProductList.as_view(),
         name='store_category_list'
     ),
-)
+]
